c70915cf0da8ab0a332d2b1d3acd9bffba2d8b72
Vlad Chyzhyk <vladchyzhyk10@gmail.com>
2024-12-25 14:53:56 +0200

    +fixed responsive rem

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,5 +3,5 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-	return <div>{children}</div>
+	return <div className='mx-auto'>{children}</div>
 }
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,5 +1,11 @@
+// import IconLogo from '@/app/assets/svg/IconLogo.svg'
+
 const Header = () => {
-	return <div className='text-[2rem] w-full h-20 bg-orange-500'>Header</div>
+	return (
+		<div className='w-[92.5rem] h-[4.875rem] items-center'>
+			<div>{/* <IconLogo /> */}</div>
+		</div>
+	)
 }
 
 export default Header
