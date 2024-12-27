357201f1c1738c308275afc2f86e80274d964ed3
Vlad Chyzhyk <vladchyzhyk10@gmail.com>
2024-12-27 17:08:55 +0200

    +added webpack

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,9 +1,19 @@
-// import IconLogo from '@/app/assets/svg/IconLogo.svg'
+import IconLocation from '@/app/assets/svg/IconLocation.svg'
+import IconLogo from '@/app/assets/svg/IconLogo.svg'
 
 const Header = () => {
+	const userLocation = 'Poland Bus Stand, Dhaka'
 	return (
 		<div className='w-[92.5rem] h-[4.875rem] items-center'>
-			<div>{/* <IconLogo /> */}</div>
+			<div>
+				<IconLogo className='w-[12.313rem] h-[2.298rem]' />
+				<div>
+					Deliver to:
+					<IconLocation className='w-[0.875rem] h-[1.563rem]' />
+					Current Location
+					{userLocation}
+				</div>
+			</div>
 		</div>
 	)
 }
